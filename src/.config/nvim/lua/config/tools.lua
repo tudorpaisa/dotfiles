d02641ad8d6b68f82f8d2e51149032cfb95380d0
SpaceWhisky <tudor.paisa@gmail.com>
2026-03-27 15:10:02 +0100

    revert adapter

--- src/.config/nvim/lua/config/tools.lua
+++ src/.config/nvim/lua/config/tools.lua
@@ -1,8 +1,8 @@
 return {
   codecompanion = {
     enabled = true,
-    adapter = "opencode",
-    model = "github-copilot/claude-sonnet-4.6",
+    adapter = "copilot",
+    model = "claude-sonnet-4.6",
     log_level = "TRACE"
   },
   mcphub = {
